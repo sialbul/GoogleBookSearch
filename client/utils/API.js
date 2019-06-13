d71d5e2e9d3a471e3dfa577586cf4e9f6b3b1893
Sibel <sbaslamisli@yahoo.com>
2019-06-13 16:22:43 -0400

    api key

--- client/utils/API.js
+++ client/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=search+terms";
-const APIKEY = "&api_key=	AIzaSyDDA4E0wBT3m8PV1SWyxi0BsQSGgw2TKBU";
+const APIKEY = "&api_key=";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
