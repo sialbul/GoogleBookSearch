1880995d97e148fe1b216638c02e86e5ebed9ed0
Sibel <sbaslamisli@yahoo.com>
2019-06-13 12:24:19 -0400

    basic setup is done

---
+++ create-react-express/client/build/precache-manifest.de2d9cec12aac362d705573514ac7bfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e35d19e454769039c31d",
+    "url": "/static/css/main.869c417b.chunk.css"
+  },
+  {
+    "revision": "e35d19e454769039c31d",
+    "url": "/static/js/main.327591c2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "bd7d6afabdb03b18231291e3bc3cb58a",
+    "url": "/index.html"
+  }
+];
